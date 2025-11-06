a87d7deb7daed3469f59d936c3bc956c966b0184
Alexandre Domingues <xanmene2000@gmail.com>
2025-11-06 11:51:37 -0300

    Adiciona as bibliotecas

---
+++ functions.py
@@ -0,0 +1,11 @@
+import numpy as np, pandas as pd, matplotlib.pyplot as plt
+import yfinance as yf
+from datetime import datetime
+from statsmodels.api import OLS, add_constant
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.stats.diagnostic import linear_reset
+from statsmodels.stats.stattools import durbin_watson
+from statsmodels.graphics.gofplots import qqplot
+import requests
+import datetime as dt
+from urllib.parse import quote
