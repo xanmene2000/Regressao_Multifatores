b392e180e2f32a0f23bd8a90f091b2c44d44134a
Alexandre Domingues <xanmene2000@gmail.com>
2025-11-06 13:10:23 -0300

    Adiciona variaveis globais

---
+++ Regressao_MultiFatores.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from functions import *
+
+# DEFINIÇÃO DE VARIAVEIS GLOBAIS
+TICKER = '' # Nome do ativo que irá no modelo (y)
+SETOR = [] # Nome dos ativos do mesmo setor
+INICIO, FIM = '2015-01-01', '2025-06-30'
