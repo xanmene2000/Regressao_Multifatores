68ea3317667552682fa32d260c365d43b02826eb
Alexandre Domingues <xanmene2000@gmail.com>
2025-11-07 16:13:51 -0300

    Adiciona fator: Brent Spot - FRED

--- Regressao_MultiFatores.py
+++ Regressao_MultiFatores.py
@@ -1,8 +1,20 @@
-import numpy as np
-import pandas as pd
 from functions import *
+import os
 
 # DEFINIÇÃO DE VARIAVEIS GLOBAIS
 TICKER = '' # Nome do ativo que irá no modelo (y)
 SETOR = [] # Nome dos ativos do mesmo setor
 INICIO, FIM = '2015-01-01', '2025-06-30'
+
+# Pega chaves das API's nas variaveis do sitema
+fred_api_key = os.getenv("FRED_API_KEY")
+if fred_api_key is None:
+    raise ValueError("Variável FRED_API_KEY não encontrada. Configure no sistema ou .env")
+
+
+# Obtem os dados do ativo alvo (y)
+
+
+# Obtem os dados dos fatores (x)
+brent = filter_data(get_api_fred(api_key=fred_api_key, series_id='DCOILBRENTEU'),start=INICIO,end=FIM)
+print(brent)
