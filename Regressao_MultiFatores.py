de6310550b95cf08c6cb7fe88eb0a1b577b9fc2d
Alexandre Domingues <xanmene2000@gmail.com>
2025-11-07 17:03:39 -0300

    Adiciona fator: UST 10Y - FRED

--- Regressao_MultiFatores.py
+++ Regressao_MultiFatores.py
@@ -4,7 +4,7 @@ import os
 # DEFINIÇÃO DE VARIAVEIS GLOBAIS
 TICKER = '' # Nome do ativo que irá no modelo (y)
 SETOR = [] # Nome dos ativos do mesmo setor
-INICIO, FIM = '2015-01-01', '2025-06-30'
+INICIO, FIM = '2014-12-31', '2025-06-30'
 
 # Pega chaves das API's nas variaveis do sitema
 fred_api_key = os.getenv("FRED_API_KEY")
@@ -16,5 +16,10 @@ if fred_api_key is None:
 
 
 # Obtem os dados dos fatores (x)
+# Usar reindex e ffill nos dados originais para alinhar com o indice do ativo alvo
 brent = filter_data(get_api_fred(api_key=fred_api_key, series_id='DCOILBRENTEU'),start=INICIO,end=FIM)
-print(brent)
+brent_ret = brent.pct_change()
+
+UST_10Y = filter_data(get_api_fred(series_id='DGS10',api_key=fred_api_key)/100,start=INICIO,end=FIM)
+UST_10Y_diff = UST_10Y.diff()
+
